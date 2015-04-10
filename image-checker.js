1ec0d78ed866ac4ce7a546c1f0533dcc4bb58d36
Adam <abaj500@york.ac.uk>
2015-04-10 16:35:53 +0100

    Changed the functionality majorily by using status codes from the http request instead of scraping the html. THis means it could with work with multiple websites. This made things alot more complicated. For example, leaving off the www. will often give you a 301 which means checking to see if something returns 'okay' 200 or 404 will fail.
    Cleaned up the code a fair bit but there are still issues and it's
    starting to get complex/hard to read. Need to chaeck the updated
    image functionality works. (via the magic of akimai)

--- image-checker.js
+++ image-checker.js
@@ -15,9 +15,11 @@ var original_image;
 var current_image;
 var content_type;
 var today;
+var result_status_code;
+var result_content_type;
 
 process.argv.forEach(function (val, index, array) {
-  if((val.indexOf('//') != -1 || val.indexOf('.com') != -1)  ){
+	 if((val.indexOf('//') != -1 || val.indexOf('.com') != -1)  ){
   	start_slice = val.indexOf('.com/')  + 4 ;
 
   	if(val.indexOf('https') > -1){
@@ -26,6 +28,10 @@ process.argv.forEach(function (val, index, array) {
 
   	if(val.indexOf('http') === -1){
   		val = 'http://'.concat(val);
+  	}
+
+  	if( val[('.com'.lenth + val.indexOf('.com') + 1 )] != '/'){		
+  		val = val.concat('/');
   	}
   	
   	if(val.indexOf('.com/') === -1){
@@ -38,97 +44,38 @@ process.argv.forEach(function (val, index, array) {
   }
 });
 
-
-function check_404(full_html){
-	$ = cheerio.load(full_html);
-	if($('title').text().indexOf('404')!= -1){
-		run_count++;
-		getSysTime();
-		console.log('\n'.concat(today).concat(clc.yellow(' Status:404 ')).concat(image_host).concat(image_path));
-		timeout_id = setTimeout(simpleHttpResquest, 20000, image_host, image_path, run_count);
-		clearInterval(second_counter);
-		second_counter = setInterval(count_second, 1000);
-	} else if($('title').text().indexOf('400')!= -1){
-		console.log('\n'.concat(today).concat(clc.red(' Status:400')).concat(' - something is wrong with the url/argument provided'));
-		console.log(clc.redBright('Terminating script.'));
-	} else {
-		clearInterval(second_counter);
-		clearTimeout(timeout_id);
-		console.log('\n'.concat(today).concat(clc.green(' Status:200 OK ')).concat(image_host).concat(image_path));
-		notifier.notify({
-			'title': 'Image Uploaded!',
-  			'message': (image_host).concat(image_path).concat(' has been uploaded.')
-		});
-		console.log(clc.greenBright('Uploaded, terminating script.'));
-		return; 
-	}
-}
-
-function checkModifiedImage(original_image, current_image){
-	if (typeof newer_image === 'undefinted'){
-		newer_image = original_image;
-	}
-	if(current_image == original_image){
-		run_count++;
-		getSysTime();
-		console.log('\n'.concat(today).concat(clc.yellow(' Status:Not updated')).concat(image_host).concat(image_path));
-		timeout_id_image = setTimeout(simpleHttpResquest, 20000, image_host, image_path, run_count);
-		clearInterval(second_counter);
-		second_counter = setInterval(count_second, 1000);
-	} else {
-		clearInterval(second_counter);
-		clearTimeout(timeout_id_image);
-		console.log('\n'.concat(today).concat(clc.green(' Status:Updated')).concat(image_host).concat(image_path));
-		console.log(clc.greenBright('Uploaded, terminating script.'));
-		return; 
-	}
-}
-
-function count_second(){
-	if(run_count > 0){ process.stdout.write(".");
-	} else{
-		process.stdout.write(" . ");
-	}
-}
-
-function getSysTime(){
-	today = new Date();
-	today = today.toLocaleString();
-	var first_slice = today.indexOf(':') - 2;
-	var last_slice = today.lastIndexOf(':') + 3;
-	today = today.slice(first_slice, last_slice);
-    return today;
-}	
-
-function simpleHttpResquest(request_host, request_path, run_count){
+function simpleHttpResquest(request_host, request_path, run_count, type_check){
 	var req = http.get(full_path, function(res) {
 		var html_response = "";
 		res.setEncoding('utf8');
-
-
 
 		res.on('data', function (chunk) {
 			html_response += chunk;
 		});
 
 		res.on('end', function(){
+			var result_headers = JSON.stringify(res.headers);
+			result_content_type = res.headers["content-type"];
+			result_status_code = res.statusCode;
+
 			if(run_count === 0){
-				console.log(clc.magenta(html_response));
+				if (result_content_type.indexOf('image') > -1){
+					console.log(clc.magenta(html_response.substring(0,250).concat('.............'))); //Truncated as image strings as very long
+				} else {
+					console.log(clc.magenta(html_response));
+				}
 			}
 
-			var result_headers = JSON.stringify(res.headers);
-			var result_content_type = res.headers["content-type"];
-
-			console.log(clc.blue());
-
-			if(result_content_type.indexOf('image') > -1){
+			
+			
+			if(result_content_type.indexOf('image') > -1 && run_count === 0){
 				if (run_count === 0){
 					original_image = html_response;
 				}
 
 				checkModifiedImage(original_image,current_image);
 			} else{
-				check_404(html_response);	
+				check404(html_response);	
 			}
 		});
 	  
@@ -144,5 +91,92 @@ function simpleHttpResquest(request_host, request_path, run_count){
 
 	req.end();
 }
+
+function check404(full_html){
+	if(run_count === 0 && result_status_code != 404){
+		getSysTime();
+		if ((result_status_code === 200) && (result_content_type.indexOf('html') == -1)){
+			console.log('\n'.concat(today).concat(clc.green(' Status:200 OK ').concat("Okay but isnt a page (could be a directory)")));
+			console.log(clc.redBright('Terminating script.'));
+			return;
+		} else if((result_status_code === 200) && (result_content_type.indexOf('html') > -1)) {
+			console.log('\n'.concat(today).concat(clc.green(' Status:200 OK ').concat("- Okay and is a page")));
+			console.log(clc.redBright('Terminating script.'));
+			return;
+		} else if(result_status_code === 301){
+			console.log('\n'.concat(today).concat(clc.green(' Status:301 OK ').concat("- A redirect to another page (you may have left off the www.)")));
+			console.log(clc.redBright('Terminating script.'));
+			return;
+		}
+	} else {
+		if(result_status_code === 404){
+			run_count++;
+			getSysTime();
+			console.log('\n'.concat(today).concat(clc.yellow(' Status:404 ')).concat(full_path));
+			timeout_id = setTimeout(simpleHttpResquest, 20000, image_host, image_path, run_count, 'check-404');
+			clearInterval(second_counter);
+			second_counter = setInterval(count_second, 1000);
+		} else {
+			clearInterval(second_counter);
+			clearTimeout(timeout_id);
+			console.log('\n'.concat(today).concat(clc.green(' Status:200 OK ')).concat(full_path));
+			notifier.notify({
+				'title': 'Image Uploaded!',
+	  			'message': (full_path).concat(' has been uploaded.')
+			}	);
+			console.log(clc.greenBright('Uploaded, terminating script.'));
+			return; 
+		}
+	}
+}
+
+function checkModifiedImage(original_image, current_image){
+	getSysTime();
+	if (typeof newer_image === 'undefinted'){
+		newer_image = original_image;
+	}
+
+	if(current_image == original_image){
+		run_count++;
+		getSysTime();
+		console.log('\n'.concat(today).concat(clc.yellow('Status:Not updated')).concat(image_host).concat(image_path));
+		timeout_id_image = setTimeout(simpleHttpResquest, 20000, image_host, image_path, run_count, 'check-update');
+		clearInterval(second_counter);
+		second_counter = setInterval(count_second, 1000);
+	} else {
+		if (run_count === 0){
+			console.log('\n'.concat(today).concat(clc.green(' Status:200 OK ').concat("Image is live.")));
+			console.log(clc.yellowBright('Terminating script.'));
+			return;
+		}
+		clearInterval(second_counter);
+		clearTimeout(timeout_id_image);
+		console.log('\n'.concat(today).concat(clc.green(' Status:Updated ')).concat(image_host).concat(image_path));
+		notifier.notify({
+			'title': 'Image Updated!',
+  			'message': (image_host).concat(image_path).concat(' has been updated.')
+		});
+		console.log(clc.greenBright('Updated, terminating script.'));
+		return; 
+	}
+}
+
+function count_second(){
+	if(run_count > 0){
+		process.stdout.write(".");
+	} else{
+		process.stdout.write(" . ");
+	}
+}
+
+function getSysTime(){
+	today = new Date();
+	today = today.toLocaleString();
+	var first_slice = today.indexOf(':') - 2;
+	var last_slice = today.lastIndexOf(':') + 3;
+	today = today.slice(first_slice, last_slice);
+    return today;
+}	
+
 
 simpleHttpResquest(image_host, image_path, run_count);
